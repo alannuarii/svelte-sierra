8795566d480d31d444942365729f182f5dddd92a
alannuarii <alannuariii@gmail.com>
2023-03-02 00:14:55 +0800

    Update

---
+++ src/routes/components/Parameter.svelte
@@ -0,0 +1,42 @@
+<script>
+	// export let suhu;
+	// export let kelembaban;
+	// export let angin;
+
+	// const satuan = '(C' + String.fromCharCode(0) + '/hari)';
+</script>
+
+<div class="d-flex justify-content-evenly align-content-center">
+	<div class="text-center">
+		<h6 class="mb-1">Suhu Udara</h6>
+		<i class="bi-thermometer-half" />
+		<p>20 C</p>
+	</div>
+	<div class="text-center">
+		<h6 class="mb-1">Kelembaban Udara</h6>
+		<i class="bi-droplet-half" />
+		<p>20 %</p>
+	</div>
+	<div class="text-center">
+		<h6 class="mb-1">Kecepatan Angin</h6>
+		<i class="bi-wind" />
+		<p>20 knot</p>
+	</div>
+</div>
+
+<style>
+	p {
+		margin: 0px;
+		font-size: 15px;
+		font-weight: 700;
+		margin-top: -5px;
+	}
+	h6 {
+		margin: 0px;
+		font-size: 13px;
+		font-weight: 400;
+	}
+	i {
+		font-size: 30px;
+	}
+</style>
--- src/routes/forecast/today/+page.svelte
+++ src/routes/forecast/today/+page.svelte
@@ -6,7 +6,7 @@
 
 	export let data;
 	const irradiance = data.data1.data;
-	const weather = data.data2.data;
+	const weather = data.data2.data.weather;
 	const pltd = data.data3.data;
 	const pv = data.data4.data;
 	const bss = data.data5.data;
