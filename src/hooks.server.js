64e60569bded1c5751e548ece3e7c75d9caa3e01
sierrawebid <sierrawebid@gmail.com>
2023-03-26 13:31:45 +0800

    Update

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 export async function handle({ event, resolve }) {
 	const token = event.cookies.get('accessToken');
 	if (!token && event.route.id?.startsWith('/(app)')) {
-		throw redirect(302, '/login');
+		throw redirect(303, '/login');
 	}
 
 	const response = await resolve(event);
