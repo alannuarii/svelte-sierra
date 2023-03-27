99d35897fee0c3df86a157839e45780e189f3c97
sierrawebid <sierrawebid@gmail.com>
2023-03-27 14:19:37 +0800

    Update

--- src/routes/(auth)/login/+page.server.js
+++ src/routes/(auth)/login/+page.server.js
@@ -38,7 +38,7 @@ export const actions = {
 			parsedData[name] = value;
 		}
 
-		const res = await fetch('https://api2.sierra.my.id/login', {
+		const res = await fetch('https://sierra.alanpy.xyz/login', {
 			method: 'POST',
 			body: JSON.stringify(parsedData),
 			headers: {
