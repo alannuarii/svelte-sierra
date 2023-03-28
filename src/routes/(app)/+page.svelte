2b81c84ac4aa8beb2c2fad4b13d130ab7153666b
sierrawebid <sierrawebid@gmail.com>
2023-03-28 11:50:00 +0800

    Update

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,40 +2,112 @@
 	import icon from '../../lib/img/icon.png';
 </script>
 
-<section class="position-relative">
-	<div
-		class="sierra position-absolute top-50 start-50 translate-middle text-center bg-secondary-subtle opacity-75 p-5"
-	>
-		<h1>S I E R R A</h1>
-		<h2>Sangihe Solar Irradiance Forecaster</h2>
+<section>
+	<h1 class="text-center mb-4">REALTIME MONITORING PLTS SANGIHE HYBRID 1,3 MWp</h1>
+	<div class="row mb-4">
+		<div class="col-2">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Frekuensi</div>
+				<div class="card-body">
+					<h5 class="card-title">50 Hz</h5>
+				</div>
+			</div>
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Tegangan R</div>
+				<div class="card-body">
+					<h5 class="card-title">20 kV</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Frekuensi</div>
+				<div class="card-body">
+					<h5 class="card-title">50 Hz</h5>
+				</div>
+			</div>
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Tegangan R</div>
+				<div class="card-body">
+					<h5 class="card-title">20 kV</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-8">
+			<div class="card w-100 h-100"></div>
+		</div>
 	</div>
-	<img src={icon} class="icon-sierra position-absolute top-0 end-0 mt-5 me-3" alt="" />
+	<div class="row">
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+				<div class="card-header">Header</div>
+				<div class="card-body">
+					<h5 class="card-title">Light card title</h5>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
 <style>
-	section {
-		background-image: url('../../lib/img/pv.jpg');
-		background-position: center center;
-		background-size: cover;
-		background-repeat: no-repeat;
-		height: 669px;
-		margin-left: -50px;
-		margin-right: -50px;
-		margin-top: -50px;
-		margin-bottom: -100px;
-		opacity: 0.8;
-	}
 	h1 {
-		font-size: 70px;
-		font-weight: 900;
-	}
-	h2 {
+		margin: 0px;
 		font-weight: 300;
-	}
-	.sierra {
-		border-radius: 70px;
-	}
-	.icon-sierra {
-		height: 100px;
 	}
 </style>
