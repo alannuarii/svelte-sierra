25ae350c302326057e3868183e92b9901873e727
sierrawebid <sierrawebid@gmail.com>
2023-03-28 11:58:44 +0800

    Update

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,36 +5,46 @@
 <section>
 	<h1 class="text-center mb-4">REALTIME MONITORING PLTS SANGIHE HYBRID 1,3 MWp</h1>
 	<div class="row mb-4">
-		<div class="col-2">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Frekuensi</div>
-				<div class="card-body">
-					<h5 class="card-title">50 Hz</h5>
+		<div class="col-5">
+			<div class="row">
+				<div class="col-12">
+					<div class="card text-bg-light mb-3">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Tegangan R</div>
-				<div class="card-body">
-					<h5 class="card-title">20 kV</h5>
+			<div class="row">
+				<div class="col-4">
+					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-2">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Frekuensi</div>
-				<div class="card-body">
-					<h5 class="card-title">50 Hz</h5>
-				</div>
-			</div>
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Tegangan R</div>
-				<div class="card-body">
-					<h5 class="card-title">20 kV</h5>
-				</div>
-			</div>
-		</div>
-		<div class="col-8">
-			<div class="card w-100 h-100"></div>
+		<div class="col-7">
+			<div class="card w-100 h-100" />
 		</div>
 	</div>
 	<div class="row">
