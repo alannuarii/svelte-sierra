8726a91e30f0733c5e079c83f2a5267e791b71bf
sierrawebid <sierrawebid@gmail.com>
2023-03-30 14:01:07 +0800

    Update

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount, onDestroy } from 'svelte';
 	import io from 'socket.io-client';
+	import Timer from '../components/Timer.svelte';
 
 	let socket;
 	let bebanPlts = 0;
@@ -44,109 +45,111 @@
 
 <section class="text-center">
 	<h1 class="mb-4 title">REALTIME MONITORING PLTS SANGIHE HYBRID 1,3 MWp</h1>
-	<div class="row mb-4">
-		<div class="col-5">
+	<div class="upper row mb-4">
+		<div class="col-4">
 			<div class="row">
 				<div class="col-12">
+					<div><Timer /></div>
 					<div class="d-flex justify-content-center">
 						<h1 class="p-4">{bebanPlts.toFixed(2)} <span>Hz</span></h1>
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-4">
-					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-						<div class="card-header">Tegangan R</div>
-						<div class="card-body">
-							<h5 class="card-title">20 <span>V</span></h5>
-						</div>
+			<div class="d-flex justify-content-evenly align-items-center">
+				<div class="card text-bg-light mb-3" style="width: 30%;">
+					<div class="card-header">Fasa R</div>
+					<div class="card-body d-flex justify-content-center align-items-center">
+						<h5 class="card-title">20 <span>V</span></h5>
 					</div>
 				</div>
-				<div class="col-4">
-					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-						<div class="card-header">Tegangan S</div>
-						<div class="card-body">
-							<h5 class="card-title">20 <span>V</span></h5>
-						</div>
+
+				<div class="card text-bg-light mb-3" style="width: 30%;">
+					<div class="card-header">Fasa S</div>
+					<div class="card-body d-flex justify-content-center align-items-center">
+						<h5 class="card-title">20 <span>V</span></h5>
 					</div>
 				</div>
-				<div class="col-4">
-					<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-						<div class="card-header">Tegangan T</div>
-						<div class="card-body">
-							<h5 class="card-title">20 <span>V</span></h5>
-						</div>
+
+				<div class="card text-bg-light mb-3" style="width: 30%;">
+					<div class="card-header">Fasa T</div>
+					<div class="card-body d-flex justify-content-center align-items-center">
+						<h5 class="card-title">20 <span>V</span></h5>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-7">
+		<div class="col-8">
 			<div class="card w-100 h-100" />
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Beban PLTS</div>
-				<div class="card-body">
-					<h5 class="card-title">650 <span>kW</span></h5>
-				</div>
+	<div class="d-flex justify-content-evenly">
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Total Beban PLTS</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">650 <span>kW</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Beban Reaktif</div>
-				<div class="card-body">
-					<h5 class="card-title">100 <span>kVAR</span></h5>
-				</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Beban Reaktif</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">100 <span>kVAR</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Beban DG 1</div>
-				<div class="card-body">
-					<h5 class="card-title">800 <span>kW</span></h5>
-				</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Beban DG 1</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">800 <span>kW</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Beban DG 2</div>
-				<div class="card-body">
-					<h5 class="card-title">850 <span>kW</span></h5>
-				</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Beban DG 2</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">850 <span>kW</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">SOC Battery 1</div>
-				<div class="card-body">
-					<h5 class="card-title">100 <span>%</span></h5>
-				</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">SOC Battery 1</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">100 <span>%</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">SOC Battery 2</div>
-				<div class="card-body">
-					<h5 class="card-title">100 <span>%</span></h5>
-				</div>
+	</div>
+	<div class="d-flex justify-content-evenly">
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">SOC Battery 2</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">100 <span>%</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Irradiance</div>
-				<div class="card-body">
-					<h5 class="card-title">500 <span>Watt/{satuanIrr}</span></h5>
-				</div>
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Beban PV Feeder 1</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">550 <span>kW</span></h5>
 			</div>
 		</div>
-		<div class="col-3">
-			<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-				<div class="card-header">Temperature</div>
-				<div class="card-body">
-					<h5 class="card-title">25 <span>{satuanTemp}</span></h5>
-				</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Beban PV Feeder 2</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">300 <span>kW</span></h5>
+			</div>
+		</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Irradiance</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">500 <span>Watt/{satuanIrr}</span></h5>
+			</div>
+		</div>
+
+		<div class="card text-bg-light mb-3" style="width: 19%;">
+			<div class="card-header">Temperature</div>
+			<div class="card-body d-flex justify-content-center align-items-center">
+				<h5 class="card-title">25 <span>{satuanTemp}</span></h5>
 			</div>
 		</div>
 	</div>
@@ -162,6 +165,16 @@
 	}
 	h1,
 	h5 {
+		margin: 0px;
 		font-weight: 700;
+	}
+	.upper {
+		height: 280px;
+	}
+	.card-body {
+		height: 50px;
+	}
+	.card{
+		border-radius: 0px;
 	}
 </style>
---
+++ src/routes/components/Timer.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { date, getToday } from '../../lib/js/date';
+
+	let time = '00:00:00';
+	let today = getToday();
+
+	setInterval(() => {
+		let date = new Date();
+		let hours = date.getHours();
+		let minutes = date.getMinutes();
+		let seconds = date.getSeconds();
+
+		hours = (hours < 10 ? '0' : '') + hours;
+		minutes = (minutes < 10 ? '0' : '') + minutes;
+		seconds = (seconds < 10 ? '0' : '') + seconds;
+
+		time = `${hours} : ${minutes} : ${seconds}`;
+	}, 1000);
+</script>
+
+<div class="text-center bg-dark text-light py-2">
+	<h5>{date(today)}</h5>
+	<span class="">{time}</span>
+</div>
+
+<style>
+	h5 {
+		margin: 0px;
+		font-size: 16px;
+	}
+	span {
+		font-size: 30px;
+        margin: 0px;
+        font-weight: 700;
+	}
+</style>
