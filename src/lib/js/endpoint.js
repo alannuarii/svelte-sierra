c3a87a5c6cbef45a7a5601db1e6c54d590d42b65
sierrawebid <sierrawebid@gmail.com>
2023-07-26 07:49:24 +0800

    Update

--- src/lib/js/endpoint.js
+++ src/lib/js/endpoint.js
@@ -1,2 +1,2 @@
-// export const API_ENDPOINT = 'https://sierrapy.alanpy.xyz'
-export const API_ENDPOINT = 'http://127.0.0.1:5000'
+export const API_ENDPOINT = 'https://sierrapy.alanpy.xyz'
+// export const API_ENDPOINT = 'http://127.0.0.1:5000'
